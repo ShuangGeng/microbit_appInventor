bb41bb3fb9138aa34e2dec4eb9d5c9088219bb41
耿爽 <shuangchenglian126@gmail.com>
2021-09-02 00:29:18 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,22 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        `)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
